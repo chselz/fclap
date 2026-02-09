c2c0614e8084c937f892a6e3f7166ea5298faf77
Christian Selzer <s6chselz@uni-bonn.de>
2026-02-09 15:28:34 +0100

    some progress
    Signed-off-by: Christian Selzer <s6chselz@uni-bonn.de>

---
+++ example/demo.f90
@@ -0,0 +1,6 @@
+program demo
+implicit none
+
+print *, "Put some examples in here!"
+end program demo
+
---
+++ src/fclap/utils/accuracy.f90
@@ -0,0 +1,28 @@
+!> Numerical storage size parameters for real and integer values
+module fclap_utils_accuracy
+   implicit none
+   public
+
+   !> Single precision real numbers
+   integer, parameter :: sp = selected_real_kind(6)
+
+   !> Double precision real numbers
+   integer, parameter :: dp = selected_real_kind(15)
+
+   !> Wanted precision
+   integer, parameter :: wp = dp
+
+   !> Char length for integers
+   integer, parameter :: i1 = selected_int_kind(2)
+
+   !> Short length for integers
+   integer, parameter :: i2 = selected_int_kind(4)
+
+   !> Length of default integers
+   integer, parameter :: i4 = selected_int_kind(9)
+
+   !> Long length for integers
+   integer, parameter :: i8 = selected_int_kind(18)
+
+
+end module fclap_utils_accuracy
